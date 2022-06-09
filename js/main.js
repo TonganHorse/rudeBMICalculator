8e2e9b2ed15ef57a1b65cbd2fd4a3ded4743fdae
tuli.kinikini@gmail.com <tuli.kinikini@gmail.com>
2022-06-09 01:13:44 -0600

    added chart as well as a delete function and button

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function calculateBMI () {
     let person = document.getElementsByName('person')
     let weight = +person[0].value
     let height = +person[1].value * 12 + +person[2].value
+
     
     let BMI = 703 * weight / height ** 2;
     
@@ -12,19 +13,32 @@ function calculateBMI () {
 }
 
 
+const anotherDisplay = document.getElementById('other-content')
+const display = document.getElementById('display-BMI')
 
 function showBMI () {
-const display = document.getElementById('display-BMI')
      let BMI = Math.round(calculateBMI())
-    console.log(BMI)
+    
+     const button = document.createElement('button')
+     button.classList.add('btn')
+     button.innerText = 'Clear'
+
     if(BMI > 24.9 && BMI < 40) {
-        display.innerText = "Your BMI is " + BMI + ", you fat back!"
+        display.innerHTML = "Your BMI is " + BMI + ", you fat back!" + '<img src="../img/bmi.jpeg">'
     } else if(BMI < 18.5) {
-        display.innerText = "Your BMI is " + BMI + ", hit the gym scrub!"
+        display.innerHTML = "Your BMI is " + BMI + ", hit the gym scrub!" + '<img src="../img/bmi.jpeg">'
     } else if(BMI >= 40){
-        display.innerText = "Your BMI is " + BMI + ", holy shit you're fat!"
-    } else {
-        display.innerText = "Your BMI is " + BMI + ", you're in range but you're still fat on the inside!"
+        display.innerHTML = "Your BMI is " + BMI + ", holy shit you're fat!" + '<img src="../img/bmi.jpeg">'
+    } else if(BMI === NaN) {
+        display.innerHTML = "You must enter a valid weight and height" + '<img src="../img/bmi.jpeg">'
+    }else {
+        display.innerHTML = "Your BMI is " + BMI + ", you're in range but you're still fat on the inside!" + '<img src="../img/bmi.jpeg">'
     }
+    anotherDisplay.append(button)
+    button.addEventListener("click", deleteContent)
+}
+function deleteContent(e) {
+    display.innerHTML = "";
+    e.target.remove()
 }
 
